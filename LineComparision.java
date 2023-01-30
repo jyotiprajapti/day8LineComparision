ce050bb5d7adfe94616d2881a038c032d11b18b2
jyotiprajapti <jyotiprajapati3029@gmail.com>
2023-01-30 17:38:35 +0530

    lines are compared

--- LineComparision.java
+++ LineComparision.java
@@ -9,13 +9,20 @@ public class LineComparision {
 		int y2 = -5;
 		int a1= 20;
 		int a2 =-10;
-		int b1 =20;
+		int b1 =10;
 		int b2 = -5;
 		
 		System.out.println("coordinates of line 1 are " + x1+ "," + x2 +"," +y1+","+  y2);
 		System.out.println("coordinates of line 1 are " + a1+ "," + a2 +"," +b1+","+  b2);
 		double line1 = Math.sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
 		double line2 = Math.sqrt(((a2-a1)*(a2-a1))+((b2-b1)*(b2-b1)));
+		
+		if (line1 == line2) {
+			System.out.println("lines are equal");
+		}
+		else {
+			System.out.println("lines are not equal");
+		}
 		
 	}
 	
